69ce128d006a586ff7e4fb02acf28709acc0dc08
ARIYAN <ARIYAN@ARIYAN-PC>
2016-02-13 21:37:15 +0530

    Grommet Specification form done.

--- app/Controller/GrommetSpecificationsController.php
+++ app/Controller/GrommetSpecificationsController.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AppController', 'Controller');
+
 /**
  * GrommetSpecifications Controller
  *
@@ -8,101 +10,105 @@ App::uses('AppController', 'Controller');
  */
 class GrommetSpecificationsController extends AppController {
 
-/**
- * Components
- *
- * @var array
- */
-	public $components = array('Paginator');
+    /**
+     * Components
+     *
+     * @var array
+     */
+    public $components = array('Paginator');
 
-/**
- * index method
- *
- * @return void
- */
-	public function index() {
-		$this->GrommetSpecification->recursive = 0;
-		$this->set('grommetSpecifications', $this->Paginator->paginate());
-	}
+    /**
+     * index method
+     *
+     * @return void
+     */
+    public function index() {
+        $this->GrommetSpecification->recursive = 0;
+        $this->set('grommetSpecifications', $this->Paginator->paginate());
+    }
 
-/**
- * view method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function view($id = null) {
-		if (!$this->GrommetSpecification->exists($id)) {
-			throw new NotFoundException(__('Invalid grommet specification'));
-		}
-		$options = array('conditions' => array('GrommetSpecification.' . $this->GrommetSpecification->primaryKey => $id));
-		$this->set('grommetSpecification', $this->GrommetSpecification->find('first', $options));
-	}
+    /**
+     * view method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function view($id = null) {
+        if (!$this->GrommetSpecification->exists($id)) {
+            throw new NotFoundException(__('Invalid grommet specification'));
+        }
+        $options = array('conditions' => array('GrommetSpecification.' . $this->GrommetSpecification->primaryKey => $id));
+        $this->set('grommetSpecification', $this->GrommetSpecification->find('first', $options));
+    }
 
-/**
- * add method
- *
- * @return void
- */
-	public function add() {
-		if ($this->request->is('post')) {
-			$this->GrommetSpecification->create();
-			if ($this->GrommetSpecification->save($this->request->data)) {
-				$this->Session->setFlash(__('The grommet specification has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The grommet specification could not be saved. Please, try again.'));
-			}
-		}
-		$items = $this->GrommetSpecification->Item->find('list');
-		$this->set(compact('items'));
-	}
+    /**
+     * add method
+     *
+     * @return void
+     */
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->GrommetSpecification->create();
+            if ($this->GrommetSpecification->save($this->request->data)) {
+                $this->Session->setFlash(__('The grommet specification has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            } else {
+                $this->Session->setFlash(__('The grommet specification could not be saved. Please, try again.'));
+            }
+        }
+        $items = $this->GrommetSpecification->Item->find('list');
+        $this->set(compact('items'));
+    }
 
-/**
- * edit method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function edit($id = null) {
-		if (!$this->GrommetSpecification->exists($id)) {
-			throw new NotFoundException(__('Invalid grommet specification'));
-		}
-		if ($this->request->is(array('post', 'put'))) {
-			if ($this->GrommetSpecification->save($this->request->data)) {
-				$this->Session->setFlash(__('The grommet specification has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The grommet specification could not be saved. Please, try again.'));
-			}
-		} else {
-			$options = array('conditions' => array('GrommetSpecification.' . $this->GrommetSpecification->primaryKey => $id));
-			$this->request->data = $this->GrommetSpecification->find('first', $options);
-		}
-		$items = $this->GrommetSpecification->Item->find('list');
-		$this->set(compact('items'));
-	}
+    /**
+     * edit method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function edit($itemId = null) {
+        $this->loadModel('Item');
+        if (!$this->Item->exists($itemId)) {
+            throw new NotFoundException(__('Invalid grommet specification'));
+        }
+        if ($this->request->is(array('post', 'put'))) {
+            if ($this->GrommetSpecification->save($this->request->data)) {
+                $this->Session->setFlash(__('The grommet specification has been saved.'), 'flash_success');
+                return $this->redirect(array('action' => 'edit', $itemId));
+            } else {
+                $this->Session->setFlash(__('The grommet specification could not be saved. Please, try again.'), 'flash_warning');
+            }
+        } else {
+            $grommet = $this->GrommetSpecification->findByItemId($itemId);
+            $grommetSpecId = $grommet['GrommetSpecification']['id'];
+            $options = array('conditions' => array('GrommetSpecification.' . $this->GrommetSpecification->primaryKey => $grommetSpecId));
+            $this->request->data = $this->GrommetSpecification->find('first', $options);
+        }
+        $this->set('itemId', $itemId);
+        $this->set('tab', 'grommet');
+    }
 
-/**
- * delete method
- *
- * @throws NotFoundException
- * @param string $id
- * @return void
- */
-	public function delete($id = null) {
-		$this->GrommetSpecification->id = $id;
-		if (!$this->GrommetSpecification->exists()) {
-			throw new NotFoundException(__('Invalid grommet specification'));
-		}
-		$this->request->allowMethod('post', 'delete');
-		if ($this->GrommetSpecification->delete()) {
-			$this->Session->setFlash(__('The grommet specification has been deleted.'));
-		} else {
-			$this->Session->setFlash(__('The grommet specification could not be deleted. Please, try again.'));
-		}
-		return $this->redirect(array('action' => 'index'));
-	}
+    /**
+     * delete method
+     *
+     * @throws NotFoundException
+     * @param string $id
+     * @return void
+     */
+    public function delete($id = null) {
+        $this->GrommetSpecification->id = $id;
+        if (!$this->GrommetSpecification->exists()) {
+            throw new NotFoundException(__('Invalid grommet specification'));
+        }
+        $this->request->allowMethod('post', 'delete');
+        if ($this->GrommetSpecification->delete()) {
+            $this->Session->setFlash(__('The grommet specification has been deleted.'));
+        } else {
+            $this->Session->setFlash(__('The grommet specification could not be deleted. Please, try again.'));
+        }
+        return $this->redirect(array('action' => 'index'));
+    }
+
 }
